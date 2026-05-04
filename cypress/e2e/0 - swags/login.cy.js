c51f889d8cc554771ed29b1fbfd64fc62f93901f
Iarlon Soares da Silva Oliveira <Ialo@academicos.com>
2026-05-04 20:43:33 -0300

    test: Adicionando estrutura do cypress

---
+++ cypress/e2e/0 - swags/login.cy.js
@@ -0,0 +1,12 @@
+describe('nome do meu teste', ()=> {
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+    });
+    
+    it('teste para login normal', () => {
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+    });
+    
+})
