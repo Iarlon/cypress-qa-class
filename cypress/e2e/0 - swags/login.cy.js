8a5c25d9f9ed3d2ae7ad406d04e891c0a90bfea2
Iarlon Soares da Silva Oliveira <Ialo@academicos.com>
2026-05-04 20:55:24 -0300

    test: adicionando assertion ao fim do test

--- cypress/e2e/0 - swags/login.cy.js
+++ cypress/e2e/0 - swags/login.cy.js
@@ -7,6 +7,7 @@ describe('nome do meu teste', ()=> {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.get('.app_logo').should('be.visible')
     });
     
 })
